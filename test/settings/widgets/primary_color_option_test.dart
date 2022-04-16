fc83384791a97bc0948f679eb7297f0f581a3ecd
roaa94 <roaa94khaddam@gmail.com>
2022-04-16 15:03:47 +0300

    remove unused import

--- test/settings/widgets/primary_color_option_test.dart
+++ test/settings/widgets/primary_color_option_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_theme_switcher/presentation/settings/widgets/primary_color_option.dart';
